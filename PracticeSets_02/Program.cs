ec9e2f8cd5964077713eae68b5beac19c1988c27
mosshead19 <garcianodianarakristy@gmail.com>
2024-08-21 11:52:12 +0800

    did the even code

---
+++ PracticeSets_02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeSets_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Write a program that prints all the even numbers from 1 to 20 using a for loop.
+             */
+            for (int i=1; i<=20;i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
