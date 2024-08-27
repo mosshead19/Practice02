993fe58d7d2fce98b6c15850457381f465aa578e
Dianara Kristy D. Garciano <142156759+mosshead19@users.noreply.github.com>
2024-08-28 04:48:34 +0800

    did 07

---
+++ PS02_07/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS02_07
+{
+    internal class Program
+    {
+        public static int CalculateDigitSum(int[] number)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                sum += number[i];
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3,4 };
+
+            Console.WriteLine(CalculateDigitSum(nums));
+            Console.ReadKey();
+        }
+    }
+}
