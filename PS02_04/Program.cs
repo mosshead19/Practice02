ad6833cdd6854352638ffb9a17a1389e69e85d90
MarkGalarpe <202380242@psu.palawan.edu.ph>
2024-08-28 15:35:39 +0800

    Added BS palindrome

--- PS02_04/Program.cs
+++ PS02_04/Program.cs
@@ -10,6 +10,27 @@ namespace PS02_04
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter a word to check if it's a palindrome");
+            string input = Console.ReadLine();
+            if (IfPalindrome(input))
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not a palindrome");
+            }
+        }
+        static bool IfPalindrome(string input)
+        {
+            string lowercase = input.ToLower();
+            int lengthInput = lowercase.Length;
+            string backwards = "";
+            for (int i = lengthInput - 1; i >= 0; i--)
+            {
+                backwards += input[i];
+            }
+            return backwards == lowercase;
         }
     }
 }
