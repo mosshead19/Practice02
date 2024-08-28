a3b16a3d8ebe55b2e47897b35157a740d9f76c3d
MarkGalarpe <202380242@psu.palawan.edu.ph>
2024-08-28 15:41:23 +0800

    Added BS sum digits

--- PS02_06/Program.cs
+++ PS02_06/Program.cs
@@ -10,6 +10,22 @@ namespace PS02_06
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int sum = SumOfDigits(number);
+
+        Console.WriteLine("The sum of the digits is: " + sum);
         }
+        static int SumOfDigits(int n)
+        {
+            int sum = 0;
+        while (n > 0)
+        {
+            sum += n % 10;
+            n /= 10;
+        }
+        }
+        return sum;
     }
 }
